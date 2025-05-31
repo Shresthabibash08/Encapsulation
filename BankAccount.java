830d996da8ae8d4986fbcb7e1d604d730a9a2b56
Shresthabibash08 <240376@softwarica.edu.np>
2025-05-31 20:24:57 +0545

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -6,20 +6,36 @@ public class BankAccount {
     public BankAccount(String AccountNumber,String accountHolderName,double balance){
         this.AccountNumber=AccountNumber;
         this.accountHolderName=accountHolderName;
-        this.balance=balance;
+        this.balance=0;
     }
-    public void deposit(String AccountNumber,double amount){
+    public void deposit(double amount){
         if(amount>0){
             balance+=amount;
+            System.out.println("Amount deposited successfully");
         }
         else{
             System.out.println("Invalid amount!!");
         }
     }
+    public void withdraw(double amount){
+        if(amount>balance){
+            System.out.println("Insufficient amount");
+        }
+        else{
+            balance-=amount;
+            System.out.println("Your amount is withdraw.");
+        }
+    }
+    public void accountDetails(){
+        System.out.println("Account Number= "+AccountNumber+" Your name is "+accountHolderName+" Your balance is "+balance);
+    }
 }
 class Action{
     public static void main(String[]args){
         BankAccount acc1=new BankAccount("1","Bibash",100000);
-        
+        acc1.deposit(1000);
+        acc1.withdraw(100);
+        acc1.accountDetails();
+
     }
 }
